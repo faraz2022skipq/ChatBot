d935370268398f8ee90d44be0b84e37ad4a161b4
Muhammad <muhammad.faraz.skipq@gmail.com>
2022-12-05 18:56:46 +0500

    bot 0.4

--- chat_bot/chat_bot_stack.py
+++ chat_bot/chat_bot_stack.py
@@ -10,7 +10,7 @@ class ChatBotStack(Stack):
         super().__init__(scope, construct_id, **kwargs)
         
         chatbot = lex.CfnBot(self, "WeatherAPI",
-            data_privacy = {"childDirected": False},
+            data_privacy = {"ChildDirected": False},
             idle_session_ttl_in_seconds = 300,
             name = "WeatherAPI",
-            role_arn = "arn:aws:iam::aws:policy/aws-service-role/LexBotPolicy")
+            role_arn = "arn:aws:iam::136037166860:role/aws-service-role/lexv2.amazonaws.com/AWSServiceRoleForLexV2Bots")

839af5b1342308a7d12c16ac26894973dc79afbf
Muhammad <muhammad.faraz.skipq@gmail.com>
2022-12-08 15:04:13 +0500

    bot 1.4

--- resources/weatherlambda.py
+++ resources/weatherlambda.py
@@ -1,10 +1,12 @@
 import urllib3
+import json
 
 http = urllib3.PoolManager()
 apitoken = "c22c535558cead35f179fe7e668cf71e"
 
-city = input()
-apiurl = "https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={APIkey}"
+city = "Lahore"
+apiurl = "https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={APIkey}&units=metric"
 cityurl = apiurl.format(cityname=city, APIkey=apitoken)
-r = http.request('GET', cityurl)
-print(r)
+requestresponse = http.request('GET', cityurl)
+data = requestresponse.data
+print(data['b'])

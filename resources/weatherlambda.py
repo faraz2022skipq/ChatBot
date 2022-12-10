ef4ac0b825a12e2d6b66dece6a0e7eb6cc6215da
Muhammad <muhammad.faraz.skipq@gmail.com>
2022-12-10 20:30:23 +0500

    bot 1.6

--- resources/weatherlambda.py
+++ resources/weatherlambda.py
@@ -2,17 +2,21 @@ import urllib3
 import json
 from types import SimpleNamespace
 
-def lambda_handler(event, context):
-    print(event)
-
 http = urllib3.PoolManager()
 apitoken = "c22c535558cead35f179fe7e668cf71e"
 
-city = "Lahore"
-apiurl = "https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={APIkey}&units=metric"
-cityurl = apiurl.format(cityname=city, APIkey=apitoken)
-requestresponse = http.request('GET', cityurl)
-data = requestresponse.data
-x = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
-temp = x.main.temp
-# return temp
+def lambda_handler(event, context):
+    # print(event)
+    
+    city = event["inputTranscript"]
+    print(city)
+    
+    apiurl = "https://api.openweathermap.org/data/2.5/weather?q={cityname}&appid={APIkey}&units=metric"
+    cityurl = apiurl.format(cityname=city, APIkey=apitoken)
+    requestresponse = http.request('GET', cityurl)
+    data = requestresponse.data
+    x = json.loads(data, object_hook=lambda d: SimpleNamespace(**d))
+    temp = x.main.temp
+    print("TEMPERATURE ", temp)
+    return temp
+    
